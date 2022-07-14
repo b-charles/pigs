e433e8b7db8f4cbfc471789ea48cebeb677feb78
Benoit Charles <19323162+b-charles@users.noreply.github.com>
2022-07-14 04:54:06 +0200

    Add Erroneous api

--- ioc/singleton.go
+++ ioc/singleton.go
@@ -12,45 +12,101 @@ func ContainerInstance() *Container {
 	return instance
 }
 
-func PutNamedFactory(factory any, name string, aliases ...any) error {
+func ErroneousPutNamedFactory(factory any, name string, aliases ...any) error {
 	return ContainerInstance().PutNamedFactory(factory, name, aliases...)
 }
 
-func PutNamed(object any, name string, aliases ...any) error {
+func ErroneousPutNamed(object any, name string, aliases ...any) error {
 	return ContainerInstance().PutNamed(object, name, aliases...)
 }
 
-func PutFactory(factory any, aliases ...any) error {
+func ErroneousPutFactory(factory any, aliases ...any) error {
 	return ContainerInstance().PutFactory(factory, aliases...)
 }
 
-func Put(object any, aliases ...any) error {
+func ErroneousPut(object any, aliases ...any) error {
 	return ContainerInstance().Put(object, aliases...)
 }
 
-func TestPutNamedFactory(factory any, name string, aliases ...any) error {
+func ErroneousTestPutNamedFactory(factory any, name string, aliases ...any) error {
 	return ContainerInstance().TestPutNamedFactory(factory, name, aliases...)
 }
 
-func TestPutNamed(object any, name string, aliases ...any) error {
+func ErroneousTestPutNamed(object any, name string, aliases ...any) error {
 	return ContainerInstance().TestPutNamed(object, name, aliases...)
 }
 
-func TestPutFactory(factory any, aliases ...any) error {
+func ErroneousTestPutFactory(factory any, aliases ...any) error {
 	return ContainerInstance().TestPutFactory(factory, aliases...)
 }
 
-func TestPut(object any, aliases ...any) error {
+func ErroneousTestPut(object any, aliases ...any) error {
 	return ContainerInstance().TestPut(object, aliases...)
 }
 
-func CallInjected(method any) {
-	err := ContainerInstance().CallInjected(method)
+func ErroneousCallInjected(method any) error {
+	return ContainerInstance().CallInjected(method)
+}
+
+func PutNamedFactory(factory any, name string, aliases ...any) {
+	err := ErroneousPutNamed(factory, name, aliases...)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func UnsureCallInjected(method any) error {
-	return ContainerInstance().CallInjected(method)
+func PutNamed(object any, name string, aliases ...any) {
+	err := ErroneousPutNamed(object, name, aliases...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func PutFactory(factory any, aliases ...any) {
+	err := ErroneousPutFactory(factory, aliases...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Put(object any, aliases ...any) {
+	err := ErroneousPut(object, aliases...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestPutNamedFactory(factory any, name string, aliases ...any) {
+	err := ErroneousTestPutNamedFactory(factory, name, aliases...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestPutNamed(object any, name string, aliases ...any) {
+	err := ErroneousTestPutNamed(object, name, aliases...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestPutFactory(factory any, aliases ...any) {
+	err := ErroneousTestPutFactory(factory, aliases...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestPut(object any, aliases ...any) {
+	err := ErroneousTestPut(object, aliases...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func CallInjected(method any) {
+	err := ErroneousCallInjected(method)
+	if err != nil {
+		panic(err)
+	}
 }
