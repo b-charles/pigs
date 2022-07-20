a119f766fe01019d011d41e2aa49f7a5521dd225
Benoit Charles <19323162+b-charles@users.noreply.github.com>
2022-07-21 00:17:58 +0200

    WIP

--- smartconf/navigable_config_test.go
+++ smartconf/navigable_config_test.go
@@ -10,15 +10,68 @@ import (
 	. "github.com/onsi/gomega"
 )
 
+func complexSetup() {
+	config.SetTest(map[string]string{
+		"co.rrup.tion":   "corruption",
+		"con.fe.ren.ce":  "conference",
+		"con.fe.ssion":   "confession",
+		"con.fu.sion":    "confusion",
+		"ga.la.xy":       "galaxy",
+		"ga.lle.ry":      "gallery",
+		"in.fec.tion":    "infection",
+		"in.qui.ry":      "inquiry",
+		"in.vest.ment":   "investment",
+		"pa.ra.dox":      "paradox",
+		"pa.ra.gra.ph":   "paragraph",
+		"per.for.ate":    "perforate",
+		"per.for.man.ce": "performance",
+	})
+}
+
 var _ = Describe("IOC registration", func() {
 
 	It("should parse empty config", func() {
 
 		config.SetTest(map[string]string{})
 
-		ioc.CallInjected(func(config *NavConfig, status *ioc.ContainerStatus) {
-			fmt.Print(status)
+		ioc.CallInjected(func(config *NavConfig) {
 			Expect(config.Keys()).To(BeEmpty())
+		})
+
+	})
+
+	It("should parse complex config", func() {
+
+		complexSetup()
+
+		ioc.CallInjected(func(config *NavConfig) {
+
+			Expect(config.Keys()).To(Equal([]string{
+				"co", "con", "ga", "in", "pa", "per",
+			}))
+
+			con := config.Child("con")
+			Expect(con.Keys()).To(Equal([]string{"fe", "fu"}))
+
+			confu := con.Child("fu")
+			Expect(confu.Keys()).To(Equal([]string{"sion"}))
+
+			confusion := confu.Child("sion")
+			Expect(confusion.Keys()).To(BeEmpty())
+
+			Expect(confusion.Value()).To(Equal("confusion"))
+
+		})
+
+	})
+
+	It("should be able to navigable", func() {
+
+		complexSetup()
+
+		ioc.CallInjected(func(config *NavConfig) {
+
+
 		})
 
 	})
